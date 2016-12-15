02d36029e06ee98fc75ab3ed23831ac8a1c011d7
Ash Wilson <ash.d.wilson@gmail.com>
2016-12-14 16:57:08 -0800

    chg: doc: Improving docstrings !minor

--- haloevents/haloevents.py
+++ haloevents/haloevents.py
@@ -8,7 +8,22 @@ from utility import Utility
 
 
 class HaloEvents(object):
+    """Initialize a CloudPassage Halo event retrieval object
 
+    Args:
+        halo_key (str): API key for CloudPassage Halo
+        halo_secret (str): API key secret for CloudPassage Halo
+
+    Keyword Args:
+        api_host (str): Hostname for Halo API.  Default is api.cloudpassage.com
+        api_port (str): Port for API endpoint.  Defaults to 443
+        start_timestamp (str): ISO8601-formatted string.  Defalults to now.
+        max_threads (int): Max number of open threads.  Defaults to 10.
+        batch_size (int): Limit the depth of the query.  Defaults to 20.
+        integration_name (str): Name of the tool using this library.
+
+
+    """
     def __init__(self, halo_key, halo_secret,
                  api_host="api.cloudpassage.com",
                  api_port=443,
@@ -29,11 +44,13 @@ class HaloEvents(object):
         self.ua = Utility.build_ua(integration_name)
 
     def __iter__(self):
+        """Yields events one at a time"""
         while True:
             for event in self.get_next_batch():
                 yield event
 
     def get_next_batch(self):
+        """Gets the next batch of events from the Halo API"""
         url_list = self.create_url_list()
         pages = self.get_pages(url_list)
         events = Utility.sorted_items_from_pages(pages, "events", "created_at")
@@ -42,6 +59,7 @@ class HaloEvents(object):
         return events
 
     def build_halo_session(self):
+        """Instantiates the Halo session"""
         halo_session = cloudpassage.HaloSession(self.halo_key,
                                                 self.halo_secret,
                                                 api_host=self.api_host,
@@ -67,6 +85,8 @@ class HaloEvents(object):
         return url_list
 
     def get_pages(self, url_list):
+        """Magic happens here... we map pages to threads in a pool, return
+        results when it's all done."""
         halo_session = self.build_halo_session()
         page_helper = cloudpassage.HttpHelper(halo_session)
         pool = ThreadPool(self.max_threads)
